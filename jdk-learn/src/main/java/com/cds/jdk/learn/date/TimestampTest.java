546b36a9ecd2cea1a410e9f1032b2142e8e6571b
anancds <anancds@163.com>
2016-06-01 11:06:53 +0800

    add date package

---
+++ jdk-learn/src/main/java/com/cds/jdk/learn/date/TimestampTest.java
@@ -0,0 +1,10 @@
+package com.cds.jdk.learn.date;
+
+import java.sql.Timestamp;
+
+public class TimestampTest {
+    public static void main(String[] args) {
+        System.out.println(Timestamp.valueOf("2016-05-30 03:57:23.000").getTime());
+        System.out.println(Timestamp.valueOf("2016-05-30 03:57:23").getTime());
+    }
+}
