e75390dc0819726f1a07069cbc92b9ad7e1291cd
anancds <anancds@163.com>
2016-07-23 08:55:03 +0800

    update

--- jdk-learn/src/main/java/com/cds/jdk/learn/date/TimestampTest.java
+++ jdk-learn/src/main/java/com/cds/jdk/learn/date/TimestampTest.java
@@ -6,5 +6,7 @@ public class TimestampTest {
     public static void main(String[] args) {
         System.out.println(Timestamp.valueOf("2016-05-30 03:57:23.000").getTime());
         System.out.println(Timestamp.valueOf("2016-05-30 03:57:23").getTime());
+
+        System.out.println((-1L) >>> 63);
     }
 }
